7aef72cb4d4917db7a7a0bf2e278973fa56eea20
Dipanjan Panja <dipanjanpanja6@gmail.com>
2021-08-10 00:21:31 +0530

    phase 2

--- src/index.js
+++ src/index.js
@@ -6,50 +6,19 @@ import ReactLeafletDriftMarker from "react-leaflet-drift-marker"
 import geojson from "./building_data.json"
 import "./styles.css"
 
-var myicon = L.icon({
-    iconUrl: "https://i.stack.imgur.com/oQJuO.png",
-
-    //       shadowUrl: "//leafletjs.com/docs/images/leaf-shadow.png",
-    iconSize: [38, 40], // size of the icon
-    //       shadowSize: [50, 64], // size of the shadow
-    //       iconAnchor: [22, 94], // point of the icon which will correspond to marker's location
-    //       shadowAnchor: [4, 62], // the same for the shadow
-    //       popupAnchor: [-3, -76], // point from which the popup should open relative to the iconAnchor
-  }),
-  parkingCar = L.icon({ iconUrl: "./greyCar.png", iconSize: [21, 40] }),
-  availableCar = L.icon({ iconUrl: "./greenCar.png", iconSize: [21, 40] }),
+var myicon = L.divIcon({ html: '<div class="pin"></div>', iconSize: [18, 18] }),
+  myicon2 = L.divIcon({ html: '<div class="pin2"></div>', iconSize: [18, 18] }),
+  myicon3 = L.divIcon({ html: '<div class="pin3"></div>', iconSize: [18, 18] }),
   index = -1,
-  ladybirdLakeIndex = -1,
+  loop2index = 22,
   highroadIndex = -1,
   reverse = false
 const timeout = 2000
 
 const getPoints = () => {
-  const ladybirdLake = [
-    // [30.24788317794715, -97.72405794590846],
-    // [30.247992078270297, -97.72399625466734],
-    // [30.2484948742885, -97.72378436066589],
-    // [30.248494875358098, -97.72370657647703],
-    // [30.2484948742885, -97.72378436066589],
-    // [30.24863621275131, -97.72372803321156],
-    // [30.249092665054217, -97.72354296069632],
-    // [30.249646427921935, -97.72367170721915],
-    // [30.250297502346925, -97.72325596430944],
-    // [30.249699716861407, -97.72154739894312],
-  ]
   const highroad = [
-    // [30.246136711149454, -97.73460499706769],
-    // [30.247177736795607, -97.73474678799832],
-    // [30.247615145669897, -97.73455438555624],
-    // [30.247440188788058, -97.73370373036116],
-    // [30.246565375674276, -97.73251889946454],
-    // [30.24562931505179, -97.73132396323885],
-    // [30.24477197739979, -97.73026068852482],
-    // [30.2448507579338, -97.72680745034108],
-    // [30.244807014351988, -97.72548088444962],
-    // [30.24473688852949, -97.72339481748863],
-    // [30.24430829287298, -97.72211880257291],
-    // [30.242129993520457, -97.72329355408458],
+    [30.341442112227067, -97.61458396007917],
+    [30.339636557667372, -97.61432323124303],
   ]
   const austin2 = [
     [30.341442112227067, -97.61458396007917],
@@ -103,21 +72,20 @@ const getPoints = () => {
   ]
 
   if (austin2.length - 1 === index) reverse = true
+  if (0 === index) reverse = false
   if (reverse) {
-    index = index + 1
+    index = index - 1
   } else {
     index = index + 1
   }
-  if (ladybirdLake.length - 1 !== ladybirdLakeIndex) ladybirdLakeIndex = ladybirdLakeIndex + 1
+  if (0 !== loop2index) loop2index = loop2index - 1
   if (highroad.length - 1 !== highroadIndex) highroadIndex = highroadIndex + 1
-
-  return { loop1: austin2[index], loop2: ladybirdLake[ladybirdLakeIndex], loop3: highroad[highroadIndex] }
+  return { loop1: austin2[index], loop2: austin2[loop2index], loop3: highroad[highroadIndex] }
 }
 const App = () => {
-  const [state, setState] = useState({ loop1: [30.341442112227067, -97.61458396007917], loop2: [30.339636557667372, -97.61432323124303], loop3: [30.42452127431796, -97.66210189454922] })
+  const [state, setState] = useState({ loop1: [30.341442112227067, -97.61458396007917], loop3: [30.339636557667372, -97.61432323124303], loop2: [30.42452127431796, -97.66210189454922] })
 
   useEffect(() => {
-    console.log("call")
     const timer = setInterval(() => {
       const { loop1, loop2, loop3 } = getPoints()
       setState({ loop1, loop2, loop3 })
@@ -136,11 +104,7 @@ const App = () => {
         zoom={17}>
         <TileLayer attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors' url="https://{s}.tile.osm.org/{z}/{x}/{y}.png" />
         <GeoJSON data={geojson} />
-        <ReactLeafletDriftMarker
-          position={state.loop1}
-          duration={2000} //keepAtCenter={true}
-          // icon={myicon}
-        >
+        <ReactLeafletDriftMarker position={state.loop1} duration={timeout} icon={myicon}>
           <Popup>
             Hello, This is to know the details about the car :). <br /> Name - Yellow car. <br /> Phone - 781-835-5258
             <br />
@@ -149,19 +113,24 @@ const App = () => {
           </Popup>
           <Tooltip>Carl L. West</Tooltip>
         </ReactLeafletDriftMarker>
-
-        {/* <Marker position={[30.341442112227067, -97.61458396007917]} icon={parkingCar}>
-          <Popup>Parking Car.</Popup>
-          <Tooltip>Parking Car.</Tooltip>
-        </Marker>
-        <Marker position={[30.339636557667372, -97.61432323124303]} icon={parkingCar}>
-          <Popup>Parking Car.</Popup>
-          <Tooltip>Parking Car.</Tooltip>
-        </Marker>
-        <Marker position={[30.42452127431796, -97.66210189454922]} icon={parkingCar}>
-          <Popup>Parking Car.</Popup>
-          <Tooltip>Parking Car.</Tooltip>
-        </Marker> */}
+        <ReactLeafletDriftMarker position={state.loop2} duration={timeout} icon={myicon2}>
+          <Popup>
+            Hello, This is to know the details about the car :). <br /> Name - Yellow car. <br /> Phone - 781-835-5258
+            <br />
+            444 Hillcrest Avenue
+            <br /> Cambridge, MA 02141
+          </Popup>
+          <Tooltip>Carl L. West</Tooltip>
+        </ReactLeafletDriftMarker>
+        <ReactLeafletDriftMarker position={state.loop3} duration={1000} icon={myicon3}>
+          <Popup>
+            Hello, This is to know the details about the car :). <br /> Name - Yellow car. <br /> Phone - 781-835-5258
+            <br />
+            444 Hillcrest Avenue
+            <br /> Cambridge, MA 02141
+          </Popup>
+          <Tooltip>Carl L. West</Tooltip>
+        </ReactLeafletDriftMarker>
       </Map>
     </div>
   )
